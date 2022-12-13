162c6d6151925c7596422866f1dae534efd163af
Luis Oliveira <luispedroliveira96@gmail.com>
2022-12-13 17:32:45 +0000

    first commit

---
+++ apps/backoffice/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'int-garage-management-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'backoffice';
+}
